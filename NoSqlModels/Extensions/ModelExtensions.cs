86c3d03090ecc86bdbe37780992319c70c152a44
Stephan Steiner <stephan.steiner@sunrise.net>
2024-07-29 17:16:41 +0200

    updated nuget packages added common extensions

---
+++ NoSqlModels/Extensions/ModelExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+namespace NoSqlModels.Extensions;
+
+public static class ModelExtensions
+{
+    public static List<Tuple<string, string>> GetDependencyProperties(this Type t)
+    {
+        return t.GetProperties()
+            .Where(prop => prop.IsDefined(typeof(DependencyFieldAttribute), true))
+            .OrderBy(n => n.Name)
+            .Select(x => new Tuple<string, string>(x.Name, x.GetCustomAttribute<DependencyFieldAttribute>(true).IdProperty))
+            .ToList();
+    }
+
+    public static List<string> GetIgnorePropertiesForObjectUpdate(this Type t, bool updateInternalFields = false, bool updateDependencyFields = false)
+    {
+        IEnumerable<PropertyInfo> properties = t.GetProperties();
+        if (updateInternalFields)
+        {
+            if (updateDependencyFields)
+                properties = properties.Where(prop => false);
+            else
+                properties = properties.Where(prop => prop.IsDefined(typeof(DependencyFieldAttribute), false));
+        }
+        else
+        {
+            if (updateDependencyFields)
+                properties = properties.Where(prop => false);
+            else
+                properties = properties.Where(prop => prop.IsDefined(typeof(DependencyFieldAttribute), false));
+        }
+        var props = properties.Select(pi => pi.Name).ToList();
+        return [.. props.OrderBy(n => n)];
+    }
+}
