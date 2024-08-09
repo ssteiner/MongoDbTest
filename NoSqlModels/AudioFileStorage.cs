e80c43d96a5a7cd4d4c4783a73d77c8855e36002
Stephan Steiner <stephan.steiner@sunrise.net>
2024-08-09 11:35:35 +0200

    added audio file operations

---
+++ NoSqlModels/AudioFileStorage.cs
@@ -0,0 +1,21 @@
+﻿namespace NoSqlModels;
+
+public class AudioFileStorage
+{
+    public string Id { get; set; }
+
+    public string FileName { get; set; }
+
+    public AudioFileType AudioType { get; set; }
+
+    public DateTime UploadDate { get; set; }
+
+    public string UploadedBy { get; set; }
+
+    public byte[] Contents { get; set; }
+}
+
+public enum AudioFileType
+{
+    Audio1, Audio2, Audio3
+}
